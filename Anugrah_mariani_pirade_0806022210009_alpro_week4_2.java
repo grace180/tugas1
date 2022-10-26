f23b2ec84152426b27a924a21f2c4d8b9dc6d60f
grace180 <115929381+grace180@users.noreply.github.com>
2022-10-26 15:24:48 +0800

    Add files via upload

---
+++ Anugrah_mariani_pirade_0806022210009_alpro_week4_2.java
@@ -0,0 +1,18 @@
+
+package anugrah_mariani_pirade_0806022210009_alpro_week4_2;
+
+import javax.swing.JOptionPane;
+
+public class Anugrah_mariani_pirade_0806022210009_alpro_week4_2 {
+
+    public static void main(String[] args) {
+        String a,b;      
+        double f,k,r;
+        int y ; 
+        a= JOptionPane.showInputDialog("masukkan suhu celcius:");
+        y= intger.parseInt(a);
+         
+        
+    }
+    
+}
